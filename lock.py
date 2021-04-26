a84f79aa74787895f7aecc44a335e23958c04685
aejb <git@ash.pm>
2021-04-26 11:40:44 +0100

    adds "all" locking, or locking by channel list

--- lock.py
+++ lock.py
@@ -23,9 +23,12 @@ class moderation(commands.Cog):
 
     async def get_lock_categories(self, guild, categories):
         lock_categories = []
-        for category in categories:
-            lock_categories.append(guild.get_channel(category))
-        return lock_categories
+        if len(categories) != 0:
+            for category in categories:
+                lock_categories.append(guild.get_channel(category))
+            return lock_categories
+        else:
+            print("Please configure the locked categories at the top of lock.py")
 
     async def status_embed(self, ctx, description):
         guild = ctx.author.guild
@@ -69,39 +72,67 @@ class moderation(commands.Cog):
 
     @commands.command()
     @commands.has_role(MOD_ROLE)
-    async def lock(self, ctx):
-        """------ locks every channel in a given category"""
+    async def lock(self, ctx, *args):
+        """------ channels by \'all\' if configured, or by a list of channels"""
         guild = ctx.author.guild
-        lock_categories = await self.get_lock_categories(guild, LOCK_CATEGORIES)
         everyone_role = discord.utils.get(guild.roles, name="@everyone")
-        lock_string = ', '.join(map(str, lock_categories))
-        embed = discord.Embed(description=f"Locking channels in {lock_string}...")
-        message = await ctx.send(embed=embed)
-        for category in ctx.author.guild.by_category():
-            if category[0] in lock_categories:
-                for channel in category[1]:
+        if len(args) == 1 and "all" in args:
+            # Lock all from LOCK_CATEGORIES
+            lock_categories = await self.get_lock_categories(guild, LOCK_CATEGORIES)
+            lock_string = ', '.join(map(str, lock_categories))
+            embed = discord.Embed(description=f"Locking channels in {lock_string}...")
+            message = await ctx.send(embed=embed)
+            for category in ctx.author.guild.by_category():
+                if category[0] in lock_categories:
+                    for channel in category[1]:
+                        await self.channel_lock(everyone_role, channel)
+            time.sleep(2)
+            embed = await self.status_embed(ctx, description=f"Locked channels in category \'{lock_string}\'")
+            await message.edit(embed = embed)
+        else:
+            lock_array = []
+            for arg in args:
+                channel = discord.utils.find(lambda m: m.mention == arg or m.name == arg, guild.text_channels)
+                if channel == None:
+                    await ctx.send(f"Channel `{arg}` not found. Please try again.")
+                else:
                     await self.channel_lock(everyone_role, channel)
-        time.sleep(2)
-        embed = await self.status_embed(ctx, description=f"Locked channels in category \'{lock_string}\'")
-        await message.edit(embed = embed)
+                    lock_array.append(channel)
+            embed = await self.status_embed(ctx, description=f"Locked channels {', '.join(map(str, lock_array))}")
+            await ctx.send(embed = embed)
+
         
     @commands.command()
     @commands.has_role(MOD_ROLE)
-    async def unlock(self, ctx):
-        """------ unlocks every channel in a given category"""
+    async def unlock(self, ctx, *args):
         guild = ctx.author.guild
-        lock_categories = await self.get_lock_categories(guild, LOCK_CATEGORIES)
+        """------ unlocks channels by \'all\' if configured, or by a list of channels"""
+        guild = ctx.author.guild
         everyone_role = discord.utils.get(guild.roles, name="@everyone")
-        lock_string = ', '.join(map(str, lock_categories))
-        embed = discord.Embed(description=f"Unlocking channels in {lock_string}...")
-        message = await ctx.send(embed=embed)
-        for category in ctx.author.guild.by_category():
-            if category[0] in lock_categories:
-                for channel in category[1]:
+        if len(args) == 1 and "all" in args:
+            lock_categories = await self.get_lock_categories(guild, LOCK_CATEGORIES)
+            everyone_role = discord.utils.get(guild.roles, name="@everyone")
+            lock_string = ', '.join(map(str, lock_categories))
+            embed = discord.Embed(description=f"Unlocking channels in {lock_string}...")
+            message = await ctx.send(embed=embed)
+            for category in ctx.author.guild.by_category():
+                if category[0] in lock_categories:
+                    for channel in category[1]:
+                        await self.channel_unlock(everyone_role, channel)
+            time.sleep(2)
+            embed = await self.status_embed(ctx, description=f"Unlocked channels in category \'{lock_string}\'")
+            await message.edit(embed = embed)
+        else:
+            lock_array = []
+            for arg in args:
+                channel = discord.utils.find(lambda m: m.mention == arg or m.name == arg, guild.text_channels)
+                if channel == None:
+                    await ctx.send(f"Channel `{arg}` not found. Please try again.")
+                else:
                     await self.channel_unlock(everyone_role, channel)
-        time.sleep(2)
-        embed = await self.status_embed(ctx, description=f"Unlocked channels in category \'{lock_string}\'")
-        await message.edit(embed = embed)
+                    lock_array.append(channel)
+            embed = await self.status_embed(ctx, description=f"Unocked channels {', '.join(map(str, lock_array))}")
+            await ctx.send(embed = embed)
 
     @commands.command()
     @commands.has_role(MOD_ROLE)
